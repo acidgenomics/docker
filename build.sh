f723196ce8a97fe8327f6189f5fff33741640937
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-15 11:48:44 -0400

    Improve shared build script

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ set -Eeuxo pipefail
 # See also:
 # - docker build --help
 # - https://docs.docker.com/engine/reference/builder/#arg
+#
+# Use `--no-cache` flag to disable build cache.
 
 package="$1"
-docker build --tag="$package" --build-arg "GITHUB_PAT=$DOCKER_GITHUB_PAT" "${HOME}/git/packages/${package}"
+docker build --tag="$package" --build-arg "GITHUB_PAT=$DOCKER_GITHUB_PAT" "${HOME}/git/docker/${package}"
 unset -v package
