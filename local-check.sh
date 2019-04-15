779c7126d2c3317a6d872b82af1cee2405ea485c
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-15 11:50:07 -0400

    Improve local check

--- local-check.sh
+++ local-check.sh
@@ -1,2 +1,2 @@
 pkg="$1"
-docker run -ti --user bioc -v "${HOME}/git/packages/${pkg}:/${pkg}" acidgenomics/basejump bash
+docker run -ti --user="bioc" --volume="${HOME}/git/packages/${pkg}:/${pkg}" --workdir="/${pkg}" acidgenomics/basejump bash
