c58210f4a1f3eecff22bf721bccd8dedd6524893
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-16 11:50:34 -0400

    Update scripts

--- local-check.sh
+++ local-check.sh
@@ -1,2 +1,3 @@
-pkg="$1"
-docker run -ti --user="bioc" --volume="${HOME}/git/packages/${pkg}:/${pkg}" --workdir="/${pkg}" acidgenomics/basejump bash
+image="$1"
+pkg="$2"
+docker run -ti --user="bioc" --volume="${HOME}/git/packages/${pkg}:/${pkg}" --workdir="/${pkg}" "$image" bash
---
+++ singlecell/cole-trapnell-lab.R
@@ -0,0 +1,25 @@
+# monocle: http://cole-trapnell-lab.github.io/monocle-release/monocle3/
+# cicero: https://cole-trapnell-lab.github.io/cicero-release/
+# garnett: https://cole-trapnell-lab.github.io/garnett/
+
+library(BiocManager)
+
+# Prepare virtual environment using reticulate.
+library(reticulate)
+virtualenv_create(envname = "reticulate")
+virtualenv_install(
+    envname = "reticulate",
+    packages = c("umap-learn", "louvain")
+)
+
+# monocle 3 alpha
+install("monocle")
+install("cole-trapnell-lab/DDRTree", ref = "simple-ppt-like")
+install("cole-trapnell-lab/L1-graph")
+install("cole-trapnell-lab/monocle-release", ref = "monocle3_alpha")
+
+# cicero
+install("cole-trapnell-lab/cicero")
+
+# garnett
+install("cole-trapnell-lab/garnett")
