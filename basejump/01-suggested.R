4fd69229e47a9cc028047aee9e85f4fc24428670
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-20 11:07:45 -0400

    Update docker

--- basejump/01-suggested.R
+++ basejump/01-suggested.R
@@ -1,9 +1,10 @@
 library(BiocManager)
 library(remotes)
+Sys.setenv("R_REMOTES_UPGRADE" = "always")
 install(c(
     "EnsDb.Hsapiens.v75",
     "gdata",
     "pzfx",
     "rio"
 ))
-install_github("tidyverse/googlesheets4", upgrade = "always")
+install_github("tidyverse/googlesheets4", dependencies = TRUE)
