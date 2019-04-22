6bed33466d7a980539088f3255e820cea32edef4
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-22 09:54:41 -0400

    Update bioc-devel

--- bioc-devel/install.R
+++ bioc-devel/install.R
@@ -4,12 +4,38 @@ suppressWarnings(
 )
 install(
     pkgs = c(
-        "remotes",
-        "devtools",
-        "roxygen2",
-        "tidyverse",
         "BiocCheck",
-        "covr"
+        "DT",
+        "Matrix",
+        "Matrix.utils",
+        "R.utils",
+        "SingleCellExperiment",
+        "backports",
+        "covr",
+        "cowplot",
+        "curl",
+        "devtools",
+        "ggrepel",
+        "knitr",
+        "magrittr",
+        "patrick",
+        "pbapply",
+        "rcmdcheck",
+        "remotes",
+        "rlang",
+        "rmarkdown",
+        "roxygen2",
+        "sessioninfo",
+        "testthat",
+        "tidyverse",
+        "viridis",
+        "xmlparsedata"
     ),
     dependencies = TRUE
+)
+library(remotes)
+install_github(
+    repo = "acidgenomics/lintr",
+    dependencies = TRUE,
+    upgrade = "always"
 )
