702dd0ec1197d40902a6504af2790f986f550267
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-04 12:58:00 -0400

    Switch bioconductor scripts to a submodule

--- bioconductor/3.5/install.R
+++
@@ -1,6 +0,0 @@
-install.packages(
-    pkgs = "BiocInstaller",
-    repos = "https://bioconductor.org/packages/3.5/bioc"
-)
-library("BiocInstaller")
-biocLite()
---
+++ submodules.sh
@@ -0,0 +1,3 @@
+git submodule add \
+    git@github.com:acidgenomics/docker-bioconductor.git \
+    bioconductor
