274a1e224bf3fa0b9d82020c2223921c978cd15c
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-04 12:21:19 -0400

    Improve Bioconductor recipes

--- basejump/bioc-3.6/install.R
+++ basejump/bioc-3.6/install.R
@@ -3,6 +3,38 @@ repos <- getOption("repos")
 repos["CRAN"] <- "https://cloud.r-project.org"
 options(repos = repos)
 
+library(BiocInstaller)
+biocLite(
+    pkgs = c(
+        "BiocCheck",
+        "DT",
+        "Matrix",
+        "Matrix.utils",
+        "R.utils",
+        "SingleCellExperiment",
+        "backports",
+        "covr",
+        "cowplot",
+        "curl",
+        "devtools",
+        "ggrepel",
+        "knitr",
+        "magrittr",
+        "patrick",
+        "pbapply",
+        "rcmdcheck",
+        "remotes",
+        "rlang",
+        "rmarkdown",
+        "roxygen2",
+        "sessioninfo",
+        "testthat",
+        "tidyverse",
+        "viridis",
+        "xmlparsedata"
+    )
+)
+
 install.packages("remotes")
 library(remotes)
 # If we attempt to set `upgrade = "always"` in `install_github()` call, it will
---
+++ bioconductor/3.5/install.R
@@ -0,0 +1,6 @@
+install.packages(
+    pkgs = "BiocInstaller",
+    repos = "https://bioconductor.org/packages/3.5/bioc"
+)
+library("BiocInstaller")
+biocLite()
