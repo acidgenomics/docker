85de6a32ad6d190c7936ce2d7920c5375ffe9d0c
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-08 06:19:55 -0400

    Improve tag latest script to pull image

--- tag-latest.sh
+++ tag-latest.sh
@@ -5,6 +5,7 @@ repo="$1"
 version="$2"
 
 docker login
+docker pull "acidgenomics/${repo}:${version}"
 docker tag "${repo}:${version}" "${repo}:latest"
 docker tag "${repo}:latest" "acidgenomics/${repo}:latest"
 docker push "acidgenomics/${repo}:latest"
