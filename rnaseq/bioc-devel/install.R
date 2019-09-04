03f741f6e9a3789cae43c5d364364dd821265cc9
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-04 13:27:59 -0400

    Add rnaseq

---
+++ rnaseq/bioc-devel/install.R
@@ -0,0 +1,27 @@
+# Install RNA-seq R packages.
+# Updated 2019-09-04.
+
+library(BiocManager)
+install(
+    pkgs = c(
+        "DESeq2",
+        "apeglm",
+        "edgeR",
+        "hexbin",
+        "limma",
+        "rhdf5"
+    )
+)
+
+library(remotes)
+install_github(
+    repo = paste(
+        "acidgenomics",
+        c(
+            "DESeqAnalysis",
+            "pfgsea"
+        ),
+        sep = "/"
+    ),
+    dependencies = TRUE
+)
