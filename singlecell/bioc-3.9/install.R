78ef544061ced9a69eb828841b0357e10528ce8a
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-04 13:36:21 -0400

    Simplify install recipe

--- singlecell/bioc-3.9/install.R
+++ singlecell/bioc-3.9/install.R
@@ -1,50 +1,28 @@
 library(BiocManager)
-library(remotes)
-
-
-
-## Satija Lab                                                               {{{1
-## =============================================================================
-
-## Install Seurat.
-if (!isTRUE("Seurat" %in% rownames(installed.packages()))) {
-    install("Seurat")
-}
-
-
-
-## Trapnell Lab                                                             {{{1
-## =============================================================================
-
-## Install monocle3 beta.
-## https://cole-trapnell-lab.github.io/monocle3/monocle3_docs/
-if (!isTRUE("monocle3" %in% rownames(installed.packages()))) {
-    install(
-        pkgs = c(
-            "DelayedMatrixStats",
-            "rgdal",
-            "sf",
-            "spatialreg"
-        )
+install(
+    pkgs = c(
+        "DelayedMatrixStats",
+        "rgdal",
+        "sf",
+        "spatialreg"
     )
-    install_github("cole-trapnell-lab/monocle3")
-}
+)
 
-## Install cicero.
-if (!isTRUE("cicero" %in% rownames(installed.packages()))) {
-    install_github("cole-trapnell-lab/cicero")
-}
-
-
-
-## Acid Genomics                                                            {{{1
-## =============================================================================
-
+library(remotes)
+install_github(
+    repo = paste(
+        "cole-trapnell-lab",
+        c(
+          "monocle3",
+          "cicero"
+        ),
+        sep = "/"
+    )
+)
 install_github(
     repo = paste(
         "acidgenomics",
         c(
-            "bcbioSingleCell",
             "Chromium",
             "pointillism"
         ),
