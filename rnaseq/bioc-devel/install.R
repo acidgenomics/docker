019efbb972ef47af98ac16fd5178f20932589776
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-03 15:12:47 -0400

    Use BiocManager consistently

--- rnaseq/bioc-devel/install.R
+++ rnaseq/bioc-devel/install.R
@@ -1,6 +1,3 @@
-# Install RNA-seq R packages.
-# Updated 2019-09-04.
-
 library(BiocManager)
 install(
     pkgs = c(
@@ -12,7 +9,9 @@ install(
         "rhdf5"
     )
 )
-
-library(remotes)
-install_github("acidgenomics/DESeqAnalysis")
-install_github("acidgenomics/pfgsea")
+install(
+    pkgs = c(
+        "acidgenomics/DESeqAnalysis",
+        "acidgenomics/pfgsea"
+    )
+)
