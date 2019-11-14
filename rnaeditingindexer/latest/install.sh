935738a829722f2aa99d7f6e763981e790e7d61b
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-14 09:40:49 -0500

    Harden Dockerfile by calling an external shell script

---
+++ rnaeditingindexer/latest/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -Eeu -o pipefail
+
+conda install \
+    bamutil=1.0.14 \
+    bedtools=2.27.1 \
+    python=2.7.16 \
+    samtools=1.9
+conda install -c cyclus java-jdk=8.0.92
+conda install -c anaconda git
+
+mkdir -p /bin/AEI
+git clone https://github.com/shalomhillelroth/RNAEditingIndexer /bin/AEI
+
+(
+    cd /bin/AEI/RNAEditingIndexer || exit 1
+    make
+)
+
+samtools faidx /bin/AEI/RNAEditingIndexer/Resources/Genomes/{HomoSapiens,MusMusculus}/*.fa
