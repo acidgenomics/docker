f26856ff6223836dc8572b49b2dd937359c4a792
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-14 10:33:02 -0500

    Improve configuration script

--- rnaeditingindexer/latest/install.sh
+++ rnaeditingindexer/latest/install.sh
@@ -2,17 +2,17 @@
 set -Eeu -o pipefail
 
 # Configure conda.
-conda install \
+conda install --yes \
     bamutil=1.0.14 \
     bedtools=2.27.1 \
     python=2.7.16 \
     samtools=1.9
-conda install -c cyclus java-jdk=8.0.92
-conda install -c anaconda git
+conda install --yes --channel=cyclus java-jdk=8.0.92
+conda install --yes --channel=anaconda git
 
 # Clone the git repo.
-mkdir -p /bin/AEI
-git clone \
+mkdir -pv /bin/AEI
+git clone --recursive \
     https://github.com/shalomhillelroth/RNAEditingIndexer \
     /bin/AEI/RNAEditingIndexer
 
