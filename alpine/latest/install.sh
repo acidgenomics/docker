ad523f90b6719f16b734fde78e07e1238522ac68
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-06 12:53:10 -0500

    Add comments

--- alpine/latest/install.sh
+++ alpine/latest/install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -Eeu -o pipefail
 
+# Alpine uses ash (Almquist shell) by default, which isn't supported yet.
+# koopa doesn't activate on login here either, just like Debian.
+
 curl -sSL https://koopa.acidgenomics.com/install | bash -s -- --shared
