d05a9f90af6821c2df890324ce5b0e63e928dd98
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-06 11:12:46 -0500

    Install latest version of R

--- ubuntu/latest/install.sh
+++ ubuntu/latest/install.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 set -Eeu -o pipefail
 
-# Need to update default R installation.
-# https://cran.r-project.org/bin/linux/ubuntu/README.html
-# Add to '/etc/apt/sources.list'
-# deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/
-
-# Note that root user ignores '/etc/profile.d/' currently and doesn't activate
-# koopa. How to fix this?
-
 apt-get update
 apt-get -y dist-upgrade
 apt-get -y install \
     curl \
     fish \
     git \
+    gnupg \
     parallel \
     sudo \
     tree \
     wget \
     zsh
+
+# Install latest version of R.
+# https://cran.r-project.org/bin/linux/ubuntu/README.html
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+if ! grep -q 'r-project.org' /etc/apt/sources.list
+then
+    echo 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/' >> /etc/apt/sources.list
+fi
+apt-get update
+apt-get -y install r-base r-base-dev
 
 rm -fr /var/lib/apt/lists/*
 rm -fr /usr/local/koopa
