e5ac112674402d558bf84c0761f17c8c8d250207
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-06 10:02:59 -0500

    Draft update to minimal Ubuntu image

--- ubuntu/latest/install.sh
+++ ubuntu/latest/install.sh
@@ -3,7 +3,17 @@ set -Eeu -o pipefail
 
 apt-get update
 apt-get -y dist-upgrade
-apt-get -y install curl git sudo
-rm -frv /usr/local/koopa
+apt-get -y install \
+    curl \
+    fish \
+    git \
+    parallel \
+    sudo \
+    tree \
+    wget \
+    zsh
+
+rm -fr /var/lib/apt/lists/*
+rm -fr /usr/local/koopa
+
 curl -sSL "https://koopa.acidgenomics.com/install" | bash -s -- --shared
-# > rm -frv /var/lib/apt/lists/*
