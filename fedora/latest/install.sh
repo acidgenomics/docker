6a9d70f9586592278822c5ddab7fbcc8aa9f3143
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-06 10:09:01 -0500

    Update install

--- fedora/latest/install.sh
+++ fedora/latest/install.sh
@@ -3,9 +3,21 @@ set -Eeu -o pipefail
 
 yum -y update
 yum -y install \
+    R \
+    chsh \
     curl \
+    fish \
     git \
     hostname \
-    which
-rm -frv /usr/local/koopa
+    man \
+    parallel \
+    top \
+    tree \
+    uptime \
+    wget \
+    which \
+    zsh
+
+rm -fr /usr/local/koopa
+
 curl -sSL "https://koopa.acidgenomics.com/install" | bash -s -- --shared
