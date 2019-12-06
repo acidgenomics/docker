40cfcd1a98ff44d8bc4f816022b5b3a65cb37647
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-06 10:06:30 -0500

    Add comments regarding R and activation

--- ubuntu/latest/install.sh
+++ ubuntu/latest/install.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -Eeu -o pipefail
 
+# Need to update default R installation.
+# https://cran.r-project.org/bin/linux/ubuntu/README.html
+# Add to '/etc/apt/sources.list'
+# deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/
+
+# Note that root user ignores '/etc/profile.d/' currently and doesn't activate
+# koopa. How to fix this?
+
 apt-get update
 apt-get -y dist-upgrade
 apt-get -y install \
