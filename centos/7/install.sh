57986910d0f7ca3f565d569b836692b0268eb318
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-18 13:53:18 -0500

    Add support for legacy CentOS releases

---
+++ centos/7/install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -Eeu -o pipefail
+
+# fish isn't currently available.
+
+# Need to fix this warning:
+# Failed to set locale, defaulting to C.
+
+# Koopa check warnings:
+# Setting LC_* failed, using "C"
+
+# Run this to enable EPEL 8, if necessary:
+# > dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
+dnf -y install \
+    dnf-plugins-core \
+    epel-release
+dnf -y config-manager --set-enabled PowerTools
+dnf -y update
+dnf -y install \
+    R \
+    curl \
+    git \
+    hostname \
+    man \
+    parallel \
+    sudo \
+    tree \
+    util-linux-user \
+    wget \
+    which \
+    zsh
+
+rm -fr /usr/local/koopa
+
+curl -sSL https://koopa.acidgenomics.com/install | bash -s -- --shared
