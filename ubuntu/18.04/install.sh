9eab82b1f1d0954a19d1c7289664e3c0e459c616
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-21 15:43:33 -0500

    Update Ubuntu default config

--- ubuntu/18.04/install.sh
+++ ubuntu/18.04/install.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -Eeux -o pipefail
 
-# FIXME Note that koopa currently doesn't activate at login for root user.
-# For some reason, scripts in '/etc/profile.d/' are currently ignored for root.
-#
-# > su -
-# Now koopa is accessible. However, the prompt doesn't change.
-
 export DEBIAN_FRONTEND="noninteractive"
-
 apt-get update
 apt-get -y install \
     bc \
@@ -24,6 +17,4 @@ curl -sSL https://koopa.acidgenomics.com/install \
 # shellcheck disable=SC1091
 source /usr/local/koopa/activate
 
-install-debian-base
-
-rm -fr /var/lib/apt/lists/*
+configure-vm
