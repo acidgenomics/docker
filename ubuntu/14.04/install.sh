35a36a043c4afa2408746af8aba449500fee5cf4
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-21 15:44:05 -0500

    Update Ubuntu install

--- ubuntu/14.04/install.sh
+++ ubuntu/14.04/install.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
-set -Eeu -o pipefail
+set -Eeux -o pipefail
 
 export DEBIAN_FRONTEND="noninteractive"
-
 apt-get update
-apt-get -y dist-upgrade
 apt-get -y install \
     bc \
     curl \
-    fish \
     git \
-    gnupg \
-    parallel \
-    sudo \
-    tree \
-    wget \
-    zsh
+    sudo
 
-rm -fr /var/lib/apt/lists/*
 rm -fr /usr/local/koopa
 
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --shared --test
+
+# shellcheck disable=SC1091
+source /usr/local/koopa/activate
+
+configure-vm
