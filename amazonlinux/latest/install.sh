88a9412c26c0418d2ec54e299aa908e131dca724
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-22 09:57:21 -0500

    Update Dockerfiles

--- amazonlinux/latest/install.sh
+++ amazonlinux/latest/install.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
-set -Eeu -o pipefail
-
-yum -y update
-yum -y install \
-    curl \
-    fish \
-    git \
-    hostname \
-    man \
-    parallel \
-    sudo \
-    tree \
-    util-linux-user \
-    wget \
-    which \
-    zsh
-# Need to enable EPEL release before we can install R here.
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum -y install R
+set -Eeux -o pipefail
 
 rm -fr /usr/local/koopa
-
-curl -sSL https://koopa.acidgenomics.com/install | bash -s -- --shared
+curl -sSL https://koopa.acidgenomics.com/install \
+    | bash -s -- --shared --test
