561ec26dd66760c502fc601d765e995854af64ac
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-22 11:15:06 -0500

    Update Debian recipes

--- debian/10/install.sh
+++ debian/10/install.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
-set -Eeu -o pipefail
+set -Eeux -o pipefail
 
-export DEBIAN_FRONTEND="noninteractive"
-apt-get update
-apt-get -y install \
-    bc \
-    curl \
-    git \
-    sudo
+# > rm -fv /root/.{bashrc,profile}
 
 rm -fr /usr/local/koopa
-
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --shared --test
-
-# shellcheck disable=SC1091
-source /usr/local/koopa/activate
-
-configure-vm
--- debian/8/install.sh
+++ debian/8/install.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
-set -Eeu -o pipefail
-
-export DEBIAN_FRONTEND="noninteractive"
-apt-get update
-apt-get -y install \
-    bc \
-    curl \
-    git \
-    sudo
+set -Eeux -o pipefail
 
 rm -fr /usr/local/koopa
-
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --shared --test
-
-# shellcheck disable=SC1091
-source /usr/local/koopa/activate
-
-configure-vm
