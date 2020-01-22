ec412a359e5dc719efc213e809a268dbd24794bd
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-22 10:25:45 -0500

    Draft fix for txdata prompt

--- ubuntu/18.04/install.sh
+++ ubuntu/18.04/install.sh
@@ -3,6 +3,9 @@ set -Eeux -o pipefail
 
 rm -fv /root/.{bashrc,profile}
 
+# Fix for tzdata prompting during non-interactive session.
+echo "America/New_York" > /etc/timezone
+
 rm -fr /usr/local/koopa
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --shared --test
