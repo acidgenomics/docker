d86164357d66599d4e7b95d564a83d92e5ef37d3
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-23 08:54:45 -0500

    Fix comment

--- ubuntu/18.04/install.sh
+++ ubuntu/18.04/install.sh
@@ -7,7 +7,7 @@ rm -fr /usr/local/koopa
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --shared --test
 
-# # shellcheck disable=SC1091
+# shellcheck disable=SC1091
 source /usr/local/koopa/activate
 
 configure-vm
