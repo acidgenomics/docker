6cc827cdf18c8dad6640dd43ae145f0437a1ca74
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-24 11:18:46 -0500

    Improve CentOS 7 recipe

--- centos/8/install.sh
+++ centos/8/install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -Eeux -o pipefail
 
+rm -fv /root/.{bash_logout,bash_profile,bashrc,cshrc,tcshrc}
+
 rm -fr /usr/local/koopa
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --shared --test
+
+# shellcheck disable=SC1091
+source /usr/local/koopa/activate
+
+configure-vm
