d30e46bae27ec1509a210e99eb8bda01f56aee1e
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-24 16:37:20 -0500

    Improve Dockerfile

--- debian/10/install.sh
+++ debian/10/install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -Eeux -o pipefail
 
-# > rm -fv /root/.{bashrc,profile}
+rm -fv /root/.{bashrc,profile}
 
 rm -fr /usr/local/koopa
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --shared --test
+
+# shellcheck disable=SC1091
+source /usr/local/koopa/activate
+
+configure-vm
