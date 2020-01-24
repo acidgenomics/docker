4d78ddba222a3b5b75cc7ca6bc2116d5ddf1faa4
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-24 16:39:46 -0500

    Improve Docker recipes

--- debian/10/install.sh
+++ debian/10/install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -Eeux -o pipefail
 
-rm -fv /root/.{bashrc,profile}
-
 rm -fr /usr/local/koopa
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --shared --test
