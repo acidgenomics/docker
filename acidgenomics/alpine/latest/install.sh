d28b33b2f2221d4b517b80b0408d4259e98437a8
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-27 09:12:14 -0500

    Work on improving Alpine config

--- acidgenomics/alpine/latest/install.sh
+++ acidgenomics/alpine/latest/install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-set -Eeux -o pipefail
+set -Eeu -o pipefail
 
 rm -fr /usr/local/koopa
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --non-interactive --test
+
+# shellcheck disable=SC1091
+source /usr/local/koopa/activate
+
+configure-vm
 
 rm -fr /tmp/*
