4ec287d21bf33750654bdf13dda680e7b15165e8
Michael Steinbaugh <mike@steinbaugh.com>
2020-03-06 11:47:38 -0500

    Ensure CLI is v2

--- acidgenomics/amzn/minimal/install.sh
+++ acidgenomics/amzn/minimal/install.sh
@@ -5,4 +5,6 @@ rm -fr /usr/local/koopa
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --non-interactive --test
 
+/usr/local/koopa/os/linux/bin/install-aws-cli
+
 rm -fr /tmp/*
