53208ddb3bafadf0903df8d5e146a54a01817441
Michael Steinbaugh <mike@steinbaugh.com>
2020-03-06 14:06:31 -0500

    Update kali

--- acidgenomics/kali/minimal/install.sh
+++ acidgenomics/kali/minimal/install.sh
@@ -5,7 +5,4 @@ rm -fr /usr/local/koopa
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --non-interactive --test
 
-# shellcheck disable=SC1091
-source /usr/local/koopa/activate
-
 rm -fr /tmp/*
