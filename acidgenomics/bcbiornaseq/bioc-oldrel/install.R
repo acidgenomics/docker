6837d1b4a8acd83599377adda2c17de2f890124a
Michael Steinbaugh <mike@steinbaugh.com>
2020-05-01 16:48:06 -0400

    Ensure we can install foreign

--- acidgenomics/bcbiornaseq/bioc-oldrel/install.R
+++ acidgenomics/bcbiornaseq/bioc-oldrel/install.R
@@ -2,4 +2,5 @@
 source("/usr/local/koopa/lang/r/include/header.R")
 stopifnot(isTRUE(nzchar(Sys.getenv("GITHUB_PAT"))))
 library(bb8)
+install.packages("https://cran.r-project.org/src/contrib/Archive/foreign/foreign_0.8-76.tar.gz")
 install("hbc/bcbioRNASeq", dependencies = TRUE)
