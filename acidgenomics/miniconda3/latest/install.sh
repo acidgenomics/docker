8512fed2d78d8d8fc09d9cfde8e2215ae8980419
Michael Steinbaugh <mike@steinbaugh.com>
2020-05-09 10:28:09 -0400

    Simplify miniconda3 recipe

--- acidgenomics/miniconda3/latest/install.sh
+++ acidgenomics/miniconda3/latest/install.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-curl -sSL https://koopa.acidgenomics.com/install \
-    | bash -s -- --non-interactive --test
-
 # shellcheck disable=SC1090
 force=1 source ~/.config/koopa/activate
 
-update-conda
+install-conda
 install-aws-cli
 install-azure-cli
 install-google-cloud-sdk
