5b749876fed86385be70ab57fb9c68667408a0e9
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-01 12:10:16 -0400

    Add wine recipes

---
+++ acidgenomics/wine-mp3tag/latest/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# See also:
+# - https://github.com/baztian/docker-wine-mp3tag
+# - https://hub.docker.com/r/scottyhardy/docker-wine/
+# - https://wiki.winehq.org/Wine_User%27s_Guide#How_to_install_and_run_Windows_programs
+
+file="mp3tagv301setup.exe"
+url="https://download.mp3tag.de/${file}"
+
+curl -O "$url"
+xvfb-run wine "$file" /S
+rm "$file"
+
+rm -fr /tmp/*
