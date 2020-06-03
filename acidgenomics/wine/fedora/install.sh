f061b5634479b018bbb0c3d643b5f3b9753d3bb3
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-03 05:46:55 -0400

    Update Fedora

--- acidgenomics/wine/fedora/install.sh
+++ acidgenomics/wine/fedora/install.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-# Note that 'winehq-stable' is currently only available on Fedora 31.
-# Can use 'winehq-devel' on Fedora 32.
+# shellcheck disable=SC1090
+force=1 source ~/.config/koopa/activate
 
-dnf -y update
-dnf -y install dnf-plugins-core
-dnf config-manager \
-    --add-repo https://dl.winehq.org/wine-builds/fedora/31/winehq.repo
-dnf -y install \
-    winehq-stable \
-    xorg-x11-apps \
-    xorg-x11-server-Xvfb \
-    xorg-x11-xauth
-
-rm -fr /tmp/*
+install-wine
