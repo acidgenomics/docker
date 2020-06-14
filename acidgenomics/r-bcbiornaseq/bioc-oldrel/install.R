8993383383ea2929eeb3c5972fe490d143acbb92
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-14 12:02:52 -0400

    mnormmt now requires R 4.0

--- acidgenomics/r-bcbiornaseq/bioc-oldrel/install.R
+++ acidgenomics/r-bcbiornaseq/bioc-oldrel/install.R
@@ -2,4 +2,8 @@
 source("/usr/local/koopa/lang/r/include/header.R")
 stopifnot(isTRUE(nzchar(Sys.getenv("GITHUB_PAT"))))
 library(bb8)
+install.packages(
+    pkgs = "https://cran.r-project.org/src/contrib/Archive/mnormt/mnormt_1.5-6.tar.gz",
+    repos = NULL
+)
 install("hbc/bcbioRNASeq", dependencies = TRUE)
