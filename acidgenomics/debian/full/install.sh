8dcdc771948bfb68e3084193b5eaa59048d6562e
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-20 21:44:27 -0400

    Update Docker image

--- acidgenomics/debian/full/install.sh
+++ acidgenomics/debian/full/install.sh
@@ -3,7 +3,17 @@
 curl -sSL https://koopa.acidgenomics.com/install \
     | bash -s -- --non-interactive --test
 
+# Switch to develop branch.
+(
+    cd ~/.config/koopa/home || exit 1
+    git fetch --all
+    git checkout -b develop origin/develop
+    git pull
+)
+
 # shellcheck disable=SC1090
 force=1 source ~/.config/koopa/activate
 
 configure-vm
+
+~/.config/koopa/home/tests/ci.sh
