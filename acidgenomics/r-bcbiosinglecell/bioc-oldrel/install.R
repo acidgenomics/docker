5b0bf2f48b8dbdb38320861ba82a5edc86871ddd
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-23 15:43:12 -0400

    Simplify Bioconductor builds

--- acidgenomics/r-bcbiosinglecell/bioc-oldrel/install.R
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env Rscript
-source("/usr/local/koopa/lang/r/include/header.R")
-stopifnot(isTRUE(nzchar(Sys.getenv("GITHUB_PAT"))))
-library(bb8)
-install("hbc/bcbioSingleCell", dependencies = TRUE)
