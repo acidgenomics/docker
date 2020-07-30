2907b719a17184962931f7142b29096c474f332d
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-30 11:46:58 -0400

    Remove nextflow recipes

--- acidgenomics/aws-batch-nextflow-nascent/latest/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC1090
-force=1 source ~/.config/koopa/activate
-
-install-aws-cli
--- acidgenomics/nextflow-atacseq/latest/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sSL https://koopa.acidgenomics.com/install \
-    | bash -s -- --non-interactive --test
