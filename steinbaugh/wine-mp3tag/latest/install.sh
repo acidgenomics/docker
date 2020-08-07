053150c67b4be100baa1595cbf9d50724a1f7cf0
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-07 16:51:54 -0400

    Convert to function

--- steinbaugh/wine-mp3tag/latest/install.sh
+++ steinbaugh/wine-mp3tag/latest/install.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-# See also:
-# - https://github.com/baztian/docker-wine-mp3tag
-# - https://hub.docker.com/r/scottyhardy/docker-wine/
-# - https://wiki.winehq.org/Wine_User%27s_Guide#How_to_install_and_run_Windows_programs
+wine_install_mp3tag() {
+    # """
+    # Install Mp3tag.
+    # @note Updated 2020-08-07.
+    #
+    # @seealso
+    # - https://github.com/baztian/docker-wine-mp3tag
+    # - https://hub.docker.com/r/scottyhardy/docker-wine/
+    # - https://wiki.winehq.org/Wine_User%27s_Guide#How_to_install_and_run_
+    #       Windows_programs
+    # """
+    file="mp3tagv302setup.exe"
+    url="https://download.mp3tag.de/${file}"
+    curl -O "$url"
+    xvfb-run wine "$file" /S
+    rm "$file"
+}
 
-file="mp3tagv302setup.exe"
-url="https://download.mp3tag.de/${file}"
-
-curl -O "$url"
-xvfb-run wine "$file" /S
-rm "$file"
-
-rm -fr /tmp/*
+wine_install_mp3tag
