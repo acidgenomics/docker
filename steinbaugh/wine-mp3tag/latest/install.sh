adf0afde34f10b52e4ac0f55cd677da0835b176b
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-07 16:49:57 -0400

    Update version

--- steinbaugh/wine-mp3tag/latest/install.sh
+++ steinbaugh/wine-mp3tag/latest/install.sh
@@ -5,7 +5,7 @@
 # - https://hub.docker.com/r/scottyhardy/docker-wine/
 # - https://wiki.winehq.org/Wine_User%27s_Guide#How_to_install_and_run_Windows_programs
 
-file="mp3tagv301setup.exe"
+file="mp3tagv302setup.exe"
 url="https://download.mp3tag.de/${file}"
 
 curl -O "$url"
