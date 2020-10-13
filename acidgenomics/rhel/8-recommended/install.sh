a34bc103e656cda1231810ebbced826bec71536c
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-13 07:35:12 -0400

    Simplify RHEL UBI build

--- acidgenomics/rhel/8-recommended/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sSL https://koopa.acidgenomics.com/install \
-    | bash -s -- --non-interactive --test
-
-# shellcheck disable=SC1090
-force=1 source ~/.config/koopa/activate
-
-configure-vm --verbose
