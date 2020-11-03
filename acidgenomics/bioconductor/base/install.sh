dd220b119b46a72f656db597e72040be58b6279f
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-03 15:15:35 -0500

    Improve Bioconductor base image

--- acidgenomics/bioconductor/base/install.sh
+++ acidgenomics/bioconductor/base/install.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-rm -fr /usr/local/koopa
-curl -sSL https://koopa.acidgenomics.com/install \
-    | bash -s -- --non-interactive --test
-
-# shellcheck disable=SC1090
-force=1 source ~/.config/koopa/activate
+source "$(koopa header bash)"
 
 configure-vm --bioconductor --verbose
