a68db4a3c4769f85dfe402021e330a40f324c53c
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-06 16:14:14 -0500

    Simplify Docker recipes

--- acidgenomics/bioconductor/3.13/install.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-
-## Temporary fix for missing source tarballs.
-## Updated 2020-11-05.
-
-library(remotes)
-install_git("https://git.bioconductor.org/packages/DelayedArray")
-install_git("https://git.bioconductor.org/packages/SingleCellExperiment")
--- acidgenomics/bioconductor/3.13/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck source=/dev/null
-source "$(koopa header bash)"
-
-install-r-devel
-update-r-config
-install-bioconductor --version='3.13'
-koopa install r
