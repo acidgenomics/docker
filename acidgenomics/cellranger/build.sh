db117afb53f75a62cf1f5abb43283da1b01d3a71
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-16 13:32:36 -0500

    Migrate cellranger

--- acidgenomics/cellranger/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-. "$(koopa header bash)"
-
-docker-build --no-push cellranger
--- acidgenomics/cellranger/latest/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -Eeu -o pipefail
-
-# shellcheck source=/dev/null
-. "$(koopa header bash)"
-
-koopa install bcl2fastq
-koopa install cellranger
