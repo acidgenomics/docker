e00dcb64119c99eb828c465d03afa8c3ddea3ad5
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-02 12:09:07 -0400

    Use single quote

--- acidgenomics/koopa/build-all-tags.sh
+++ acidgenomics/koopa/build-all-tags.sh
@@ -2,4 +2,4 @@
 
 koopa app docker build-all-tags \
     --local="${HOME}/.config/koopa/docker/acidgenomics/koopa" \
-    --remote="public.ecr.aws/acidgenomics/koopa"
+    --remote='public.ecr.aws/acidgenomics/koopa'
