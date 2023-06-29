0a309b8039ab65c248f37b578eecd07f4926d401
Michael Steinbaugh <mike@steinbaugh.com>
2023-06-29 12:31:54 -0400

    Add a comment

--- acidgenomics/koopa/build-all-tags.sh
+++ acidgenomics/koopa/build-all-tags.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# To build a single distro:
+# > koopa app docker build \
+# >     --local="${HOME}/.config/koopa/docker/acidgenomics/koopa/debian" \
+# >     --remote='public.ecr.aws/acidgenomics/koopa:debian'
+
 koopa app docker build-all-tags \
     --local="${HOME}/.config/koopa/docker/acidgenomics/koopa" \
     --remote='public.ecr.aws/acidgenomics/koopa'
