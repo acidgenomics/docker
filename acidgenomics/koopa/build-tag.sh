54d3e73e2307db04b7dae8b19a56afc8a8c22fcd
Michael Steinbaugh <mike@steinbaugh.com>
2024-06-23 15:15:23 -0400

    Allow looping

--- acidgenomics/koopa/build-tag.sh
+++ acidgenomics/koopa/build-tag.sh
@@ -4,14 +4,21 @@
 source "$(koopa header bash)"
 
 main() {
-    local -A dict
-    dict['distro']="$(koopa_read 'Distro' '')"
-    dict['local']="${HOME}/monorepo/docker/acidgenomics/koopa/${dict['distro']}"
-    dict['remote']="public.ecr.aws/acidgenomics/koopa:${dict['distro']}"
-    koopa_assert_is_dir "${dict['local']}"
-    koopa app docker build \
-        --local="${dict['local']}" \
-        --remote="${dict['remote']}"
+    local distro
+    koopa_assert_has_args "$#"
+    for distro in "$@"
+    do
+        local -A dict
+        dict['distro']="$distro"
+        dict['local']="${HOME}/monorepo/docker/acidgenomics/\
+koopa/${dict['distro']}"
+        dict['remote']="public.ecr.aws/acidgenomics/koopa:${dict['distro']}"
+        koopa_assert_is_dir "${dict['local']}"
+        koopa app docker build \
+            --local="${dict['local']}" \
+            --remote="${dict['remote']}"
+        return 0
+    done
     return 0
 }
 
