2df2407591dc45ffcd6be3bae986b877a86474f5
Michael Steinbaugh <mike@steinbaugh.com>
2024-06-23 15:06:38 -0400

    Add script to build tags manually

---
+++ acidgenomics/koopa/build-tag.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# shellcheck source=/dev/null
+source "$(koopa header bash)"
+
+main() {
+    local -A dict
+    dict['distro']="$( \
+        koopa_read \
+            'Distro' \
+            "${dict['distro']}" \
+    )"
+    dict['local']="${HOME}/monorepo/docker/acidgenomics/koopa/${dict['distro']}"
+    dict['remote']="public.ecr.aws/acidgenomics/koopa:${dict['distro']}"
+    koopa_assert_is_dir "${dict['local']}"
+    koopa app docker build \
+        --local="${dict['local']}" \
+        --remote="${dict['remote']}"
+    return 0
+}
+
+main "$@"
