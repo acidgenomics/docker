b23d0e7535c3fae7b4b710d48e32180d4292e678
Michael Steinbaugh <mike@steinbaugh.com>
2024-07-15 18:09:34 -0400

    Rework recipe

--- acidgenomics/r-base/build-all-tags.sh
+++ acidgenomics/r-base/build-all-tags.sh
@@ -2,5 +2,5 @@
 set -Eeuo pipefail
 
 koopa app docker build-all-tags \
-    --local="${HOME}/.config/koopa/docker/acidgenomics/r-base" \
+    --local="${PWD:?}" \
     --remote='public.ecr.aws/acidgenomics/r-base'
